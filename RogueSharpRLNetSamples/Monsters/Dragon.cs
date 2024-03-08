7f92179f6381e3dd934b4b5182843908f18b731c
Raisybear <elias.spycher@gmail.com>
2024-03-08 10:10:44 +0100

    Dragon

---
+++ RogueSharpRLNetSamples/Monsters/Dragon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RogueSharpRLNetSamples.Monsters
+{
+    internal class Dragon
+    {
+    }
+}
