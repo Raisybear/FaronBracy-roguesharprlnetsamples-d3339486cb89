ef9425fa237f75d8bfa73793757c0506008681be
robin_sacher <robin.sacher@gmx.ch>
2024-03-15 10:43:24 +0100

    Dragon update leben und Gold + Schwierigkeit

--- RogueSharpRLNetSamples/Systems/ActorGenerator.cs
+++ RogueSharpRLNetSamples/Systems/ActorGenerator.cs
@@ -4,47 +4,56 @@ using RogueSharpRLNetSamples.Monsters;
 
 namespace RogueSharpRLNetSamples.Systems
 {
-   public static class ActorGenerator
-   {
-      private static Player _player = null;
+    public static class ActorGenerator
+    {
+        private static Player _player = null;
 
-      public static Monster CreateMonster( int level, Point location )
-      {
-         Pool<Monster> monsterPool = new Pool<Monster>();
-         monsterPool.Add( Kobold.Create( level ), 25 );
-         monsterPool.Add( Ooze.Create( level ), 25 );
-         monsterPool.Add( Goblin.Create( level ), 50 );
-         monsterPool.Add( Dragon.Create( level ), 100 ); 
+        public static Monster CreateMonster(int level, Point location)
+        {
+            Pool<Monster> monsterPool = new Pool<Monster>();
+            if (level < 10)
+            {
+                monsterPool.Add(Kobold.Create(level), 25);
+                monsterPool.Add(Ooze.Create(level), 25);
+                monsterPool.Add(Goblin.Create(level), 50);
+                monsterPool.Add(Dragon.Create(level), 100);
+            }
 
-         Monster monster = monsterPool.Get();
-         monster.X = location.X;
-         monster.Y = location.Y;
+            else if(level == 10)
+            {
+                monsterPool.Add(Dragon.Create(level), 100);
+            }
 
-         return monster;
-      }
+            Monster monster = monsterPool.Get();
+            monster.X = location.X;
+            monster.Y = location.Y;
+
+            return monster;
+        }
 
 
-      public static Player CreatePlayer()
-      {
-         if ( _player == null )
-         {
-            _player = new Player {
-               Attack = 2,
-               AttackChance = 50,
-               Awareness = 15,
-               Color = Colors.Player,
-               Defense = 2,
-               DefenseChance = 40,
-               Gold = 0,
-               Health = 25,     // 100
-               MaxHealth = 25,  // 100
-               Name = "Ala",    // Rogue
-               Speed = 10,
-               Symbol = '@'
-            };
-         }
+        public static Player CreatePlayer()
+        {
+            if (_player == null)
+            {
+                _player = new Player
+                {
+                    Attack = 2,
+                    AttackChance = 50,
+                    Awareness = 15,
+                    Color = Colors.Player,
+                    Defense = 2,
+                    DefenseChance = 40,
+                    Gold = 0,
+                    Health = 25,     // 100
+                    MaxHealth = 25,  // 100
+                    Name = "Ala",    // Rogue
+                    Speed = 10,
+                    Symbol = '@'
+                };
+            }
 
-         return _player;
-      }
-   }
+            return _player;
+        }
+    }
 }
